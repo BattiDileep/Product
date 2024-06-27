c092b90b2e7a1dbfba0756063625bdcd345d6e54
dileep <battidileep6@gmail.com>
2024-06-27 21:46:43 +0530

    second commit

--- src/test/java/com/amazon/product/Product2ApplicationTests.java
+++ src/test/java/com/amazon/product/Product2ApplicationTests.java
@@ -1,13 +1,37 @@
 package com.amazon.product;
 
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.Mockito.mockitoSession;
+import static org.mockito.Mockito.when;
+
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.mockito.session.MockitoSessionBuilder;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import com.amazon.product.entity.Product;
+import com.amazon.product.repo.ProductRepo;
+import com.amazon.product.service.ProductService;
 
 @SpringBootTest
 class Product2ApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
+@Test
+	public void addProductTest_01() {
+		ProductRepo mockrepo=Mockito.mock(ProductRepo.class);
+		
+		Product p=new Product();
+		p.setProductId((long) 10);
+		p.setProductName("mobile");
+		p.setProductQty((long) 10);
+		p.setProductStatus("succuss");
+		
+		
+		Mockito.when(mockrepo.save(p)).thenReturn(p);
+		assertNotNull(p);
+		
+		}
+			
+	
+	
 
 }
